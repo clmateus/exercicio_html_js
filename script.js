be6c7c4da290bf40d5fd544ddb8066063ad8ddbb
clmateus <cl.mateuscosta@gmail.com>
2025-02-19 18:11:54 -0300

    added ids in index.html and prevent default treatment in script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnEnviar = document.querySelector('#btn-enviar')
+
+btnEnviar.addEventListener('click', function(e){
+    e.preventDefault()
+
+    const campoNumero1 = document.querySelector('#input-numero-1')
+    const campoNumero2 = document.querySelector('#input-numero-2')
+})
