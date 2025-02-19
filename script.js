2e7cdac0efc242df39f9dd33fc49bb6d8c4e068b
clmateus <cl.mateuscosta@gmail.com>
2025-02-19 18:26:53 -0300

    script.js updated. function comparaNumeros and btnEnviar function updated.

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const btnEnviar = document.querySelector('#btn-enviar')
 
+function comparaNumeros(num1, num2) {
+    if(num1 > num2) {
+        return false
+    } else { 
+        return true
+    }
+}
+
 btnEnviar.addEventListener('click', function(e){
     e.preventDefault()
 
     const campoNumero1 = document.querySelector('#input-numero-1')
     const campoNumero2 = document.querySelector('#input-numero-2')
+    const campoMensagem = document.querySelector('.div-mensagem')
+    const resultado = comparaNumeros(campoNumero1.value, campoNumero2.value)
+
+    if(resultado){
+        campoMensagem.innerHTML += `<p>asd</p>`
+    } else {
+        campoMensagem.innerHTML += `<p>cvxvx</p>`
+    }
 })
